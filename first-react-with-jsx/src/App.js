829e541e98ee6e4f9e74796093b6a9a2b8b07d57
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-05 21:28:45 -0800

    Added content to App.js that is displayed on the html

--- first-react-with-jsx/src/App.js
+++ first-react-with-jsx/src/App.js
@@ -7,17 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World!!!</h1>
+        <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Master MERN</li>
+          <li>Get a Job that I love</li>
+          <li>Max out my Borderlands 3 Characters</li>
+        </ul>
       </header>
     </div>
   );
